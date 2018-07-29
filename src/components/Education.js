dcf3e406cddb3c1a5da324bdd01f0b92578f7ef3
Dariusz Sibik <sibikdariusz@gmail.com>
2018-07-29 14:45:52 +0200

    Add Education component and Award Component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,12 +1,44 @@
-
+import { translate, Trans } from 'react-i18next';
 import React, { Component } from 'react';
 
-export default class Education extends Component {
+class Education extends Component {
   render() {
+    const { t, i18n } = this.props;
+    const language = t("language");
+
     return (
 
-            <h2> Education </h2>
+      <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+      <div className="my-auto">
+          <h2 className="mb-5">{t('education')}</h2>
+
+          <div className="resume-item d-flex flex-column flex-md-row mb-5">
+              <div className="resume-content mr-auto">
+                  <h3 className="mb-0">{t("education_university")}</h3>
+                  <div className="subheading mb-3">{t("education_university_degree")}</div>
+                  <div>{t("education_university_specialisation")}</div>
+                  <p>{t("education_university_diploma")}</p>
+              </div>
+              <div className="resume-date text-md-right">
+                  <span className="text-primary">{t("education_university_date")}</span>
+              </div>
+          </div>
+
+          <div className="resume-item d-flex flex-column flex-md-row mb-5">
+              <div className="resume-content mr-auto">
+                  <h3 className="mb-0">{t("eitca")}</h3>
+                  <div className="subheading mb-3">{t("eitca-is")}</div>
+              </div>
+              <div className="resume-date text-md-right">
+                  <span className="text-primary">{t("eitca-date")}</span>
+              </div>
+          </div>
+
+      </div>
+  </section>
         
     );
   }
 }
+
+export default translate('translations')(Education);
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,7 +5,26 @@ export default class Experience extends Component {
   render() {
     return (
 
-            <h2> Experience </h2>
+      <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+      <div className="my-auto">
+          <h2 className="experience-title mb-2.5">Experience</h2>
+
+          <div className="resume-item d-flex flex-column flex-md-row mb-5">
+              <div className="resume-content mr-auto">
+                  <h3 className="mb-0">Web Developer</h3>
+                  <div className="subheading mb-3">Jagiellonian University in Kraków</div>
+                  <p>• Administration of Application Liferay 6.2.5 • Creation pages in Liferay based on structures and templates • Creation  templates for Velocity Web Pages • Participation in the project of the new login system (technologies used: Javascript, Jquery, HTML, CSS) • Participation in the project for editors: http://www.uj.edu.pl/web/wsw-test/start • Participation in the creation of a website for the Public Procurement Department • Participation in the creation and updating of JS scripts and validation of the login form 
+ 
+ Pages preapered during Internship 1) Erasmus  DWM – http://www.erasmus.dwm.uj.edu.pl/ 2) Ingarden Conference –  http://www.ingarden.confer.uj.edu.pl/ 3) Blog of the Jagiellonian Library -  http://www.blog.bj.uj.edu.pl/ 4) International Exchange Department –  http://www.dwm.uj.edu.pl/  </p>
+              </div>
+              <div className="resume-date text-md-right">
+                  <span className="text-primary">07/2017 - Present</span>
+              </div>
+          </div>
+
+      </div>
+
+  </section>
         
     );
   }
