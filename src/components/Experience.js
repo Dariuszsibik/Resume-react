8264c004edc7b46854a17ca3ce3928bf76d53d46
Dariuszsibik <sibikdariusz@gmail.com>
2018-08-06 10:34:02 +0200

    Update Experience.js
    Update Experience Component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,31 +1,36 @@
-
 import React, { Component } from 'react';
+import { translate, Trans } from 'react-i18next';
 
-export default class Experience extends Component {
+class Experience extends Component {
   render() {
+    const { t, i18n } = this.props;
+    const language = t("language");
+
     return (
 
       <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
       <div className="my-auto">
-          <h2 className="experience-title mb-2.5">Experience</h2>
+          <h2 className="experience-title mb-2.5">{t('experience')}</h2>
 
           <div className="resume-item d-flex flex-column flex-md-row mb-5">
               <div className="resume-content mr-auto">
-                  <h3 className="mb-0">Web Developer</h3>
-                  <div className="subheading mb-3">Jagiellonian University in Kraków</div>
+                  <h3 className="mb-0">{t('experience_position')}</h3>
+                  <div className="subheading mb-3">{t('experience_uj')}</div>
                         <ul class="fa-ul mb-0">
                             <li>
-                                <i class="fa-li fa fa-check"></i> Praca nad projektami dla Uniwersytetu Jagiellońskiego, opartymi na platformie Liferay 6.2.5.</li>
+                                <i class="fa-li fa fa-check"></i> {t('experience_uj_responsibilities1')} </li>
                             <li>
-                                <i class="fa-li fa fa-check"></i> Frontendu aplikacji: HTML, CSS, Bootstrap, JavaScript, jQuery.</li>
+                                <i class="fa-li fa fa-check"></i> {t('experience_uj_responsibilities2')}</li>
                             <li>
-                                <i class="fa-li fa fa-check"></i> Backendu aplikacji: Java, Velocity.</li>
+                                <i class="fa-li fa fa-check"></i> {t('experience_uj_responsibilities3')}</li>
                             <li>
-                                <i class="fa-li fa fa-check"></i> Implementacja wymaganych zmian, poprawa bieżących błędów, oraz aktualizacja kodu w zdalnym repozytorium Gitlab.</li>
+                                <i class="fa-li fa fa-check"></i> {t('experience_uj_responsibilities4')}</li>
                             <li>
-                                <i class="fa-li fa fa-check"></i> Tworzenie stron w Liferay na podstawie struktur i szablonów.</li>
+                                <i class="fa-li fa fa-check"></i> {t('experience_uj_responsibilities5')}</li>
                             <li>
-                                <i class="fa-li fa fa-check"></i> Automatyzacja własnej pracy.</li>
+                                <i class="fa-li fa fa-check"></i> {t('experience_uj_responsibilities6')}</li>
+                            <li>
+                                <i class="fa-li fa fa-check"></i> {t('experience_uj_responsibilities7')}</li>
                         </ul>
               </div>
               <div className="resume-date text-md-right">
@@ -36,7 +41,9 @@ export default class Experience extends Component {
       </div>
 
   </section>
-        
+
     );
   }
 }
+
+export default translate('translations')(Experience);
